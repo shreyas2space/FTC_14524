e5f363ad2c9299ae307684a1b3b0191eb7cc480a
kjn01 <91856362+kjn01@users.noreply.github.com>
2022-01-13 21:15:49 -0600

    Update autov1.java

--- autov1.java
+++ autov1.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import java.util.concurrent.TimeUnit;
 
 @com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "autov1", group = "Autonomous")
 public class autov1 extends LinearOpMode {
@@ -14,20 +15,43 @@ public class autov1 extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         robot.initialize(hardwareMap);
-//        testMotor = hardwareMap.dcMotor.get("testMotor");
-//
-//        testMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        testMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         waitForStart();
 
-        moveForward(1, 10000, 5.0);
+//        moveForward(1, 10000, 5.0);
+        Thread thread1 = new Thread() {
+            public void run() {
+                moveCarousel(1, 10000, 5.0);
+                moveForward(1, 3200, 5.0);
+                moveTurn(1, 1250, 5.0, true);
+                moveForward(1, 2600, 5.0);
+                moveArm(.05, 750, 2);
+                moveArm(.05,-750, 2);
+                moveTurn(1, 250, 1.0, false);
+                moveForward(1, -2700, 5.0);
+            }
+        };
+
+        Thread thread2 = new Thread() {
+            public void run() {
+                try {
+                    TimeUnit.SECONDS.sleep(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                moveIntake(1, 1000000000, 22.5);
+            }
+        };
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+//        moveArm(1, 1000, 5.0);
+//        moveIntake(1, 10000, 5.0);
     }
     public void moveForward(double speed, double distance_counts, double timeout) {
-//        encoderDrive(speed, distance_counts, timeout, robot.frontRight);
-//        encoderDrive(speed, distance_counts, timeout, robot.backRight);
-//        encoderDrive(speed, distance_counts, timeout, robot.frontLeft);
-//        encoderDrive(speed, distance_counts, timeout, robot.backLeft);
 
         int targetfl;
         int targetfr;
@@ -93,10 +117,6 @@ public class autov1 extends LinearOpMode {
     }
 
     public void moveStrafe(double speed, double distance_counts, double timeout, boolean left) {
-//        encoderDrive(speed, distance_counts, timeout, robot.frontRight);
-//        encoderDrive(speed, distance_counts, timeout, robot.backRight);
-//        encoderDrive(speed, distance_counts, timeout, robot.frontLeft);
-//        encoderDrive(speed, distance_counts, timeout, robot.backLeft);
 
         int targetfl;
         int targetfr;
@@ -179,10 +199,6 @@ public class autov1 extends LinearOpMode {
     }
 
     public void moveTurn(double speed, double distance_counts, double timeout, boolean left) {
-//        encoderDrive(speed, distance_counts, timeout, robot.frontRight);
-//        encoderDrive(speed, distance_counts, timeout, robot.backRight);
-//        encoderDrive(speed, distance_counts, timeout, robot.frontLeft);
-//        encoderDrive(speed, distance_counts, timeout, robot.backLeft);
 
         int targetfl;
         int targetfr;
@@ -205,9 +221,9 @@ public class autov1 extends LinearOpMode {
                 targetbr = robot.backRight.getCurrentPosition() + (int)distance_counts;
             }
             robot.frontLeft.setTargetPosition(targetfl);
-            robot.frontRight.setTargetPosition(targetfl);
-            robot.backLeft.setTargetPosition(targetfl);
-            robot.backRight.setTargetPosition(targetfl);
+            robot.frontRight.setTargetPosition(targetfr);
+            robot.backLeft.setTargetPosition(targetbl);
+            robot.backRight.setTargetPosition(targetbr);
 
             robot.frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             robot.frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -260,6 +276,121 @@ public class autov1 extends LinearOpMode {
             robot.frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             robot.backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             robot.backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        }
+    }
+
+    public void moveCarousel(double speed, double distance_counts, double timeout) {
+
+        int targetcarousel;
+
+        if (opModeIsActive()) {
+
+            targetcarousel = (int) (robot.carouselMotor.getCurrentPosition() + (speed*(int)distance_counts));
+            robot.carouselMotor.setTargetPosition(targetcarousel);
+
+            robot.carouselMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            runtime.reset();
+            robot.carouselMotor.setPower(speed);
+
+
+            while(opModeIsActive() &&
+                    (runtime.seconds() < timeout) &&
+                    (robot.carouselMotor.isBusy())) {
+
+                telemetry.addData("Current carousel position", robot.carouselMotor.getCurrentPosition());
+
+                telemetry.update();
+            }
+
+            robot.carouselMotor.setPower(0.1*speed);
+            sleep(100);
+            robot.carouselMotor.setPower(0);
+
+            robot.carouselMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            robot.carouselMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        }
+    }
+
+    public void moveIntake(double speed, double distance_counts, double timeout) {
+
+        int targetleft;
+        int targetright;
+
+        if (opModeIsActive()) {
+
+            targetright = robot.leftIntake.getCurrentPosition() + (int)distance_counts;
+            targetleft = robot.rightIntake.getCurrentPosition() + (int)distance_counts;
+            robot.leftIntake.setTargetPosition(targetleft);
+            robot.rightIntake.setTargetPosition(targetright);
+
+            robot.leftIntake.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rightIntake.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            runtime.reset();
+            robot.leftIntake.setPower(speed);
+            robot.rightIntake.setPower(speed);
+
+
+            while(opModeIsActive() &&
+                    (runtime.seconds() < timeout) &&
+                    (robot.leftIntake.isBusy())) {
+
+                telemetry.addData("Current left intake position", robot.leftIntake.getCurrentPosition());
+                telemetry.addData("Current right intake position", robot.rightIntake.getCurrentPosition());
+
+                telemetry.update();
+            }
+
+            robot.leftIntake.setPower(0.1);
+            robot.rightIntake.setPower(0.1);
+            sleep(100);
+            robot.leftIntake.setPower(0);
+            robot.rightIntake.setPower(0);
+
+            robot.leftIntake.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.rightIntake.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            robot.leftIntake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.rightIntake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        }
+    }
+
+    public void moveArm(double speed, double distance_counts, double timeout) {
+
+        int target;
+
+        if (opModeIsActive()) {
+
+            target = robot.armMotor.getCurrentPosition() + (int)distance_counts;
+            robot.armMotor.setTargetPosition(target);
+
+            robot.armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            runtime.reset();
+            robot.armMotor.setPower(speed);
+
+
+            while(opModeIsActive() &&
+                    (runtime.seconds() < timeout) &&
+                    (robot.armMotor.isBusy())) {
+
+                telemetry.addData("Current arm position", robot.armMotor.getCurrentPosition());
+
+                telemetry.update();
+            }
+
+            robot.armMotor.setPower(0.1);
+            sleep(100);
+            robot.armMotor.setPower(0);
+
+            robot.armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            robot.armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         }
     }
