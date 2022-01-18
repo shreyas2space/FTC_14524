c1c027c4981625abf506d32d891ec0f2a6cc7ba0
shreyas2space <56804035+shreyas2space@users.noreply.github.com>
2022-01-17 21:21:52 -0600

    Update mecanumTeleOpv2.java

--- mecanumTeleOpv2.java
+++ mecanumTeleOpv2.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import java.lang.Math;
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "mecanumTeleOpv2", group = "TeleOp")
 public class mecanumTeleOpv2 extends LinearOpMode {
@@ -16,6 +17,8 @@ public class mecanumTeleOpv2 extends LinearOpMode {
             double y = -gamepad1.left_stick_y;
             double x = gamepad1.left_stick_x;
             double rx = -gamepad1.right_stick_x;
+            
+            double servoPos = 0.0
 
             robot.frontLeft.setPower((-y + x + rx)/2);
             robot.backLeft.setPower((-y - x + rx)/2);
@@ -27,6 +30,12 @@ public class mecanumTeleOpv2 extends LinearOpMode {
             robot.armMotor.setPower(gamepad2.right_stick_y/3);
 
             robot.carouselMotor.setPower(gamepad2.left_trigger);
+            
+            if (gamepad2.x) {
+                servoPos = Math.abs(servoPos-1.0);
+            }
+            
+            robot.armServo.setPosition(servoPos);
 
             idle();
         }
