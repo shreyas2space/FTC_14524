6c04308a4f71d294e5a868ae4f82e86cd8d1f373
kjn01 <91856362+kjn01@users.noreply.github.com>
2022-01-24 17:53:13 -0600

    Update mecanumTeleOpv2.java

--- mecanumTeleOpv2.java
+++ mecanumTeleOpv2.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import java.lang.Math;
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "mecanumTeleOpv2", group = "TeleOp")
 public class mecanumTeleOpv2 extends LinearOpMode {
@@ -15,27 +14,29 @@ public class mecanumTeleOpv2 extends LinearOpMode {
         waitForStart();
         while(opModeIsActive()) {
             double y = -gamepad1.left_stick_y;
-            double x = gamepad1.left_stick_x;
+            double x = -gamepad1.left_stick_x;
             double rx = -gamepad1.right_stick_x;
-            
-            double servoPos = 0.0;
 
-            robot.frontLeft.setPower((-y + x + rx)/2);
+//            double servoPos = 0.0;
+
+            robot.frontLeft.setPower((-y + x + rx)/2*.9);
             robot.backLeft.setPower((-y - x + rx)/2);
-            robot.frontRight.setPower((-y - x - rx)/2);
+            robot.frontRight.setPower((-y - x - rx)/2*.9);
             robot.backRight.setPower((-y + x - rx)/2);
 
-            robot.leftIntake.setPower(gamepad2.left_stick_y/2.5);
-            robot.rightIntake.setPower(gamepad2.left_stick_y/2.5);
-            robot.armMotor.setPower(gamepad2.right_stick_y/3);
+            robot.leftIntake.setPower(gamepad2.left_stick_y*.5);
+            robot.rightIntake.setPower(gamepad2.left_stick_y*.5);
+            robot.armMotor.setPower(gamepad2.right_stick_y*0.3);
 
             robot.carouselMotor.setPower(gamepad2.left_trigger);
-            
-            if (gamepad2.x) {
-                servoPos = Math.abs(servoPos-1.0);
-            }
-            
-            robot.armServo.setPosition(servoPos);
+            robot.carouselMotor.setPower(-gamepad2.right_trigger);
+
+
+//            if (gamepad2.x) {
+//                servoPos = Math.abs(servoPos-1.0);
+//            }
+
+//            robot.armServo.setPosition(servoPos);
 
             idle();
         }
